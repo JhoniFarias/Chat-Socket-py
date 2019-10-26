9cab68b23a44cbe1df3a201d6f715576ce903d46
JhoniFarias <jhonifarias16@gmail.com>
2019-10-26 20:58:26 -0300

    Primeira versão do chat em python via socket

---
+++ cliente.py
@@ -0,0 +1,69 @@
+﻿import socket
+from socket import AF_INET, SOCK_STREAM
+from threading import Thread
+from tkinter import *
+
+frame_ip = Tk()
+frame_ip.title("Digite o endereco IP para se Conectar")
+
+def altera_janela():
+    frame_ip.quit()
+    
+
+
+host = Entry(frame_ip)
+host.pack()
+
+btnEnviar = Button(frame_ip, text="Conectar", command=altera_janela)
+btnEnviar.pack()
+frame_ip.geometry("200x200")
+frame_ip.mainloop()
+
+
+PORT = 5000
+ADDR = (host.get(), PORT)
+
+client_socket = socket.socket(AF_INET, SOCK_STREAM)
+client_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+client_socket.connect(ADDR)
+
+
+def recebe_mensagem():
+    while True:
+        try:
+            msg = client_socket.recv(2048).decode("utf8")
+            listaMensagem.insert(END, msg)
+        except OSError:  
+            break
+
+
+def enviar_mensagem(event=None):
+    msg = inputMensagem.get()
+    inputMensagem.delete(0, END)
+    client_socket.send(bytes(msg, "utf8"))
+
+
+janela = Tk()
+janela.title("Comunicação Criptografada")
+
+
+container_mensagem = Frame(janela)
+
+
+listaMensagem = Listbox(container_mensagem, height=15, width=50)
+listaMensagem.pack(side=LEFT, fill=BOTH)
+listaMensagem.pack()
+container_mensagem.pack()
+
+inputMensagem = Entry(janela)
+
+inputMensagem.bind("<Return>", enviar_mensagem)
+inputMensagem.pack()
+btnEnviar = Button(janela, text="Enviar", command=enviar_mensagem)
+btnEnviar.pack()
+
+recebe_mensagemThread = Thread(target=recebe_mensagem)
+recebe_mensagemThread.start()
+
+
+janela.mainloop()
---
+++ servidor.py
@@ -0,0 +1,39 @@
+﻿from socket import *
+import threading
+
+HOST = ''
+PORT = 5000
+s = socket(AF_INET, SOCK_STREAM)
+s.bind((HOST, PORT))
+s.listen(10)
+
+listaClient = set()
+
+
+def envia_cliente(client_conn):
+    while True:
+
+        data = client_conn.recv(1024)
+        if(not data):
+            print(client_conn.getpeername(), 'saiu')
+            return
+
+        print(data)
+        for c in listaClient:
+            c.send(data)
+
+
+print("AGUARDANDO CONEXÃO")
+while True:
+    conn, addr = s.accept()
+    print('Cliente Conectado', addr)
+    listaClient.add(conn)
+    threading.Thread(target=envia_cliente, args=(conn,)).start()
+
+
+while True:
+    data = conn.recv(1024)
+    print("Recebido -  ", repr(data))
+    conn.sendall(data)
+conn.close()
+input()
